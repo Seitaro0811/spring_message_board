50339526dd9b4d1474c1388a8b216a48fb1268ce
seitaro.ogiri <seitaro.0811@icloud.com>
2020-07-30 02:02:45 +0900

    Initial commit

---
+++ src/test/java/message_board/MessageBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package message_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
