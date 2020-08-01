ca6c652d20d7971fe30c0cdf81ceb3d644ceb1fa
seitaro.ogiri <seitaro.0811@icloud.com>
2020-08-01 17:12:47 +0900

    add sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO messages
+ (title, name, content)
+ VALUES
+ ('サンプルタイトル１', 'サンプルA', 'サンプル文章');
+INSERT INTO messages
+ (title, name, content)
+ VALUES
+ ('サンプルタイトル２', 'サンプルB', 'サンプル文章');
