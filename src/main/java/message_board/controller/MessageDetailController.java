8896ccd2e0f6c8783f7ef5415bb5eb7377a912f6
seitaro.ogiri <seitaro.0811@icloud.com>
2020-08-01 17:14:00 +0900

    add MessageDetail

---
+++ src/main/java/message_board/controller/MessageDetailController.java
@@ -0,0 +1,27 @@
+package message_board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import message_board.model.Message;
+import message_board.service.MessageService;
+
+@Controller
+public class MessageDetailController {
+	@Autowired
+	MessageService service;
+	
+	@RequestMapping(value="/detail/{id}", method=RequestMethod.GET)
+	public ModelAndView showMessage(@PathVariable("id") Integer id, ModelAndView mav) {
+		Message message = service.findById(id);
+		mav.setViewName("message_detail");
+		mav.addObject("message", message);
+		
+		return mav;
+	}
+
+}
---
+++ src/main/java/message_board/service/MessageService.java
@@ -0,0 +1,37 @@
+package message_board.service;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import message_board.model.Message;
+import message_board.model.MessageForm;
+import message_board.repository.MessageRepository;
+
+@Service
+public class MessageService {
+	@Autowired
+	MessageRepository repository;
+	
+	public List<Message> showAllMessage() {
+		List<Message> messages = repository.findAll();
+		return messages;
+	}
+	
+	public void newMessage(MessageForm form) {
+		Message newMessage = new Message();
+		newMessage.setTitle(form.getTitle());
+		newMessage.setName(form.getName());
+		newMessage.setContent(form.getContent());
+		Timestamp current_time = new Timestamp(System.currentTimeMillis());
+		newMessage.setCreated_at(current_time);
+		newMessage.setUpdated_at(current_time);
+		repository.saveAndFlush(newMessage);
+	}
+	
+	public Message findById(Integer id) {
+		return repository.getOne(id);
+	}
+}
