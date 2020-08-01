b0d74834bffd13bf5f6d7fddb256d3c2f494b1c9
seitaro.ogiri <seitaro.0811@icloud.com>
2020-08-01 17:09:34 +0900

    modified index

--- src/main/java/message_board/controller/IndexController.java
+++ src/main/java/message_board/controller/IndexController.java
@@ -1,16 +1,34 @@
 package message_board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import message_board.model.MessageForm;
+import message_board.service.MessageService;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
+	@Autowired
+	MessageService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("messages", service.showAllMessage());
+		
+		return mav;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView submitMessage(@ModelAttribute MessageForm form, ModelAndView mav) {
+		service.newMessage(form);
+		mav.setViewName("index");
+		mav.addObject("messages", service.showAllMessage());
 		
 		return mav;
 	}
